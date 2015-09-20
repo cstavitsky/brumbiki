5bec82048b584ff8e14db7177d28e31e532e19a2
Gray Edstrom & Nathan Vanderlaan <pair+grayedstrom.nvanderlaan@devbootcamp.com>
2015-09-20 12:32:28 -0500

    Refactor tweet.rb file and collected/create mentioned twitter users
    Refactored the tweet.rb file to prepare for one-degree implementation. Also started collecting all mentioned users from target's tweets, and creating new twitter_user objects from them. Finally, another call to twitter is made to collect those users' profile information

--- app/models/twitter_user.rb
+++ app/models/twitter_user.rb
@@ -3,13 +3,14 @@ class TwitterUser
 
   def initialize(attributes)
     @uid = attributes[:uid]
+    @handle = attributes[:handle]
     @name = attributes[:name]
     @profile_image = attributes[:profile_image]
     @user_type = attributes.fetch(:user_type, "current")
   end
 
   def self.locate_target(tweets)
-    p tweets  
+    
   end
 
   def self.top_three_mentions(tweets)
