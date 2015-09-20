3f6d0f86bd373462dcac2af724dd1fbed7c6b241
Gray Edstrom & Nathan Vanderlaan <pair+grayedstrom.nvanderlaan@devbootcamp.com>
2015-09-20 15:54:28 -0500

    Add current, target, and tertiary users
    Added module for twitter api client calls. Create and return current user info along with target and tertiary users for the one-degree drawing/feature.

--- app/controllers/twitter_users_controller.rb
+++ app/controllers/twitter_users_controller.rb
@@ -3,10 +3,11 @@ class TwitterUsersController < ApplicationController
   def index
     attributes = { uid: current_user.uid, name: current_user.name, profile_image: current_user.profile_image }
     current_twitter_user = TwitterUser.new(attributes)
-    p "CURRENT USER:" + current_user.name
-    TwitterUser.locate_target(current_user.target_tweets)
+    target_twitter_user = TwitterUser.locate_target(current_user.target_tweets)
+    tertiary_twitter_users = TwitterUser.top_three_mentions(current_user.target_tweets)
+    everything = [current_twitter_user, target_twitter_user] + tertiary_twitter_users
 
-    # render json: TwitterUser.xxxxxxx.to_json
+    render json: everything
   end
 
 end
