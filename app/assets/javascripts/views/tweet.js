33f645e68d2a102c0cf9a0e8e8f262662d2b7390
Dylan Andrews <dylanandr@gmail.com>
2015-09-21 13:28:21 -0500

    Clean up backbone models and init.js
    Large functions have been refactored and are more readable. Search result model has initialize function to handle the logic of modifying the twitter handle. An @ is now put in front of every handle regardless of whether or not the user used an @ in the search bar.

--- app/assets/javascripts/views/tweet.js
+++ app/assets/javascripts/views/tweet.js
@@ -1,7 +1,10 @@
 var TweetView = Backbone.View.extend({
+  className: "tweet",
   template: JST["templates/tweet"],
+
 
   render: function() {
     this.$el.html(this.template(this.model.attributes));
   }
 });
+
