5e852e952dba5290e25f971025fa3beb558fdf15
Nathan Vanderlaan <email": null>
2015-09-20 21:35:36 -0500

    Display one degree profile images and begin site layout/css design
    Developed front end models for the one degree drawing. Made adjustments to index page html divs, classes, and ids to prepare for overall design studies.

--- app/assets/javascripts/views/twitter_users.js
+++ app/assets/javascripts/views/twitter_users.js
@@ -8,13 +8,19 @@ var TwitterUsersView = Backbone.View.extend({
     var twitterUserView = new TwitterUserView({model: twitterUser});
     twitterUserView.render();
 
-    $("#one-degree-drawing-container").append(twitterUserView.el);
+    if (twitterUser.attributes.user_type != "tertiary") {
+      $("#current-target-container").append(twitterUserView.el);
+      $("#current-target-container").append(twitterUserView.el);
+    } else {
+      $("#third-degree-container").append(twitterUserView.el);
+    }
+
     $("#one-degree-drawing-container").css("display", "block");
   },
 
   addAll: function() {
     $("#one-degree-button-container").css("display", "none");
-    
+
     return this.collection.each(function(twitterUser) {
       return this.addOne(twitterUser);
     }, this);
