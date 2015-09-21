a904857da15440ae09f2b97bbbfb56c803e04e68
Nathan Vanderlaan <email": null>
2015-09-21 13:43:03 -0500

    Continue site design

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   var keywords = []
   var tweet = ""
-  $("#keyword-search-container").hide();
-
   var tweets = new TweetsCollection();
   var tweetsView = new TweetsView({ collection: tweets });
+
+  $("#keyword-search-container").hide();
 
   $("form").on("submit", function(event) {
     event.preventDefault();
@@ -23,31 +23,18 @@ $(document).ready(function() {
     $("#search-results-container").empty();
   });
 
-  $("#link-search-button").on("click", function(event){
-    event.preventDefault();
-
-    var query = $("#link-search-bar").val();
-    var results = new SearchResultsCollection();
-    var resultsCollectionView = new SearchResultsView({ collection: results});
-
-    results.fetch({
-      reset: true,
-      data: $.param({ query: query })
-    });
-  });
-
-  $("#link-search-button").on("click", function(event){
-    event.preventDefault();
-
-    var query = $("#link-search-bar").val();
-    var results = new SearchResultsCollection();
-    var resultsCollectionView = new SearchResultsView({ collection: results});
-
-    results.fetch({
-      reset: true,
-      data: $.param({ query: query })
-    });
-  });
+  // $("#link-search-button").on("click", function(event){
+  //   event.preventDefault();
+  //
+  //   var query = $("#link-search-bar").val();
+  //   var results = new SearchResultsCollection();
+  //   var resultsCollectionView = new SearchResultsView({ collection: results});
+  //
+  //   results.fetch({
+  //     reset: true,
+  //     data: $.param({ query: query })
+  //   });
+  // });
 
   $("#tweets-container").on("click", ".keyword", function(event){
     event.preventDefault();
