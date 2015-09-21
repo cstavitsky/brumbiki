2531185657e892a6d71148a94991b381e898150f
Chris Stavitsky <corridorr@gmail.com>
2015-09-21 13:35:21 -0500

    Add nonworking groundwork for draggable and droppable images

---
+++ app/assets/javascripts/dragging.js
@@ -0,0 +1,14 @@
+// $(function () {
+//         $(".profile-circles").draggable();
+//         $(".profile-circles").droppable({
+//                 drop: function (event, ui) {
+//                         $("#top-container").html("dropped!");
+//                 },
+//                 over: function (event, ui) {
+//                         $("#top-container").html("moving in!");
+//                 },
+//                 out: function (event, ui) {
+//                         $("#top-container").html("moving out!");
+//                 }
+//         });
+// });
